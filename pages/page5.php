498c3cbc81aa4ae759d3fd796b64760c6caf5cc5
Davi Monteiro <montsgit@gmail.com>
2025-04-02 17:10:30 -0300

    Title Color

--- pages/page5.php
+++ pages/page5.php
@@ -1,28 +1,28 @@
 <div class="space-y-4">
     <div class="bg-white p-4 shadow-md rounded-lg flex items-start space-x-4">
         <div>
-            <h2 class="text-lg font-bold text-blue-700">Página 5.1</h2>
+            <h2 class="text-lg font-bold text-orange-500">Página 5.1</h2>
             <p class="text-gray-600 text-sm">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi accusantium iste blanditiis, cupiditate non reiciendis sed, odio ipsum voluptates officiis sint nisi itaque dolorem. Voluptate eligendi cum veritatis at recusandae?</p>
         </div>
     </div>
 
     <div class="bg-white p-4 shadow-md rounded-lg flex items-start space-x-4">
         <div>
-            <h2 class="text-lg font-bold text-blue-700">Página 5.2</h2>
+            <h2 class="text-lg font-bold text-orange-500">Página 5.2</h2>
             <p class="text-gray-600 text-sm">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi accusantium iste blanditiis, cupiditate non reiciendis sed, odio ipsum voluptates officiis sint nisi itaque dolorem. Voluptate eligendi cum veritatis at recusandae?</p>
         </div>
     </div>
 
     <div class="bg-white p-4 shadow-md rounded-lg flex items-start space-x-4">
         <div>
-            <h2 class="text-lg font-bold text-blue-700">Página 5.3</h2>
+            <h2 class="text-lg font-bold text-orange-500">Página 5.3</h2>
             <p class="text-gray-600 text-sm">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi accusantium iste blanditiis, cupiditate non reiciendis sed, odio ipsum voluptates officiis sint nisi itaque dolorem. Voluptate eligendi cum veritatis at recusandae?</p>
         </div>
     </div>
 
     <div class="bg-white p-4 shadow-md rounded-lg flex items-start space-x-4">
         <div>
-            <h2 class="text-lg font-bold text-blue-700">Página 5.4</h2>
+            <h2 class="text-lg font-bold text-orange-500">Página 5.4</h2>
             <p class="text-gray-600 text-sm">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi accusantium iste blanditiis, cupiditate non reiciendis sed, odio ipsum voluptates officiis sint nisi itaque dolorem. Voluptate eligendi cum veritatis at recusandae?</p>
         </div>
     </div>
